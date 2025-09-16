04a32f3ff96dcfd01f623802972c257c2a9f0985
notmadeofglitter-hue <not.made.of.glitter@gmail.com>
2025-09-15 21:17:17 -0300

    empty

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,6 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 
 export const metadata: Metadata = {
@@ -56,9 +53,7 @@ export default function RootLayout({
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
-          <Footer />
-          <Analytics />
-          <SpeedInsights />
+
         </main>
       </body>
     </html>
